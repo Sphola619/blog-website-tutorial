7b11df028dd44f35465ee40a324021302a3013b3
Sipho <sisusasipho@gmail.com>
2026-02-01 18:37:45 +0200

    register and login pages

--- client/src/pages/RegisterPage.js
+++ client/src/pages/RegisterPage.js
@@ -1,9 +1,9 @@
-export default function LoginPage() {
+export default function RegisterPage() {
   return (
     <form action="">
       <input type="text" placeholder="Username"/>
       <input type="password" placeholder="Password"/>
-      <button>Login</button>
+      <button>Register</button>
     </form>
   );
 }
